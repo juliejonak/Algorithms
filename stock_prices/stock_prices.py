d3b0381d33130620e3436753bd2a8fa640602692
Julie Jonak <juliejonak10@gmail.com>
2019-07-17 12:22:27 -0500

    First pass solution stock_prices passes

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,32 +2,44 @@
 
 import argparse
 
-def find_max_profit(prices):
-  pass
-
 # receives list of prices
 # returns the maximum profit made from a single buy and sell
 # find profit by subtracting the buy price from the sell price
 
+def find_max_profit(prices):
+    # Edge case: prices does not contain enough elements
+    if len(prices) < 2:
+        return "Error: Price list must contain at least two prices."
 
-# set prices[0] at the first curr_min to test against following prices
-# store a max_profit
-# iterate through the list
-# inner loop, iterate through following list items
-# test if prices[j] - curr_min is > max_profit
-# if greater than, update max profit
+    # store a max_profit
+    max_profit = float("-inf")
 
-# optional: store prices that lead to max profit in case information is useful 
+    # optional: store prices that lead to max profit in case information is useful 
+    buy_price = 0
+    sell_price = 0
 
-# edge cases? must handle possible negative max_profit. empty price list. non-integer item in list.
+    # iterate through the list
+    for i in range(0, len(prices)-1):
+        try:
+            # set curr_min to first item in prices, iterate through until second to last item
+            curr_min = prices[i]
 
+            # inner loop, iterate through following list items
+            for j in range(i+1, len(prices)):
+                # test if prices[j] - curr_min is > max_profit
+                if prices[j] - curr_min > max_profit:
+                    # if greater than, update max profit
+                    max_profit = prices[j] - curr_min
+                    buy_price = curr_min
+                    sell_price = prices[j]
 
+                    print(f"To maximize profit, you should buy at {buy_price} and sell at {sell_price} for ${max_profit} in profit.")
+        except:
+            return "Invalid data in list. Integers only."
 
+    return max_profit
 
-
-
-print(find_max_profit([10, 7, 5, 8, 11, 9])) #6
-
+# Nested loop, O(n^2) time efficiency
 
 
 # if __name__ == '__main__':
