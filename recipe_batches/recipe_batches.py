a2de253571c3c6137e589546e269298ffd07b6da
Julie Jonak <juliejonak10@gmail.com>
2019-07-17 17:18:48 -0500

    Adds psuedocode for recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,7 +2,25 @@
 
 import math
 
+# will receive two dictionaries
+# first dict is the recipe to create a batch
+# second dict is available ingredients
+# return max number of whole batches that can be made from ingredients using that recipe
+
 def recipe_batches(recipe, ingredients):
+  # edge case: if ingredients is empty, return 0
+  # store a batch count
+
+  # while items in ingredients:
+    # for loop iterates over ingredients:
+      # if recipe[i] not in ingredients:
+        # return 0
+      # elif recipe[i] in ingredients:
+        # subtract recipe[i] amount from ingredients
+        # if subtracted amount below 0:
+          # return batch_count
+        # else: 
+          # update ingredients amount to subtracted total
   pass 
 
 
