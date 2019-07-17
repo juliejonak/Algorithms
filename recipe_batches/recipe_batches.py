47b40c8af27cd93e1b250d624a2f23df3491a665
Julie Jonak <juliejonak10@gmail.com>
2019-07-17 18:02:21 -0500

    Solves recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -9,24 +9,38 @@ import math
 
 def recipe_batches(recipe, ingredients):
   # edge case: if ingredients is empty, return 0
-  # store a batch count
+  if len(ingredients) == 0:
+    return 0
 
-  # while items in ingredients:
-    # for loop iterates over ingredients:
-      # if recipe[i] not in ingredients:
-        # return 0
-      # elif recipe[i] in ingredients:
-        # subtract recipe[i] amount from ingredients
+  # store a batch count
+  batch_count = 0
+
+  while len(ingredients) != 0:
+    for item, amount in recipe.items():
+      # if recipe[i] in ingredients:
+      if item in ingredients:
         # if subtracted amount below 0:
-          # return batch_count
-        # else: 
+        if ingredients[item] - recipe[item] < 0:
+            return batch_count
+        else: 
           # update ingredients amount to subtracted total
-  pass 
+          ingredients[item] = ingredients[item] - recipe[item]
+      
+      # else if recipe[i] not in ingredients:
+      else:
+        return 0
+
+    batch_count += 1
+  
+  return batch_count
+
+# Alg runs through N (n based on recipe) * number of batches
+# O(n) run time
 
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  ingredients = { 'milk': 432, 'butter': 153, 'flour': 51 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
