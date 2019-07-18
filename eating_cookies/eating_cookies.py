893ad3a62611d1f2a794e6b6a36c1f7c228d4ffa
Julie Jonak <juliejonak10@gmail.com>
2019-07-18 17:37:06 -0500

    Adds optimized eating_cookies solution, passes tests

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,10 +5,7 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-    # Store possible ways to eat the cookie as options to iterate through for solutions [1, 2, 3]
-    ways_to_eat = [1, 2, 3]
-  
+def eating_cookies1(n, cache=None):
     # if there are no cookies, return 1 (only one way to eat 0 cookies)
     if n == 0:
         return 1
@@ -17,15 +14,31 @@ def eating_cookies(n, cache=None):
         return 0
 
     # the count is equal to the sum of the count of n-1, n-2, and n-3 (all possible ways to start eating the cookies)
-    count = eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    count = (eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache))
 
     return count
-
-print(eating_cookies(4))
 
 # Could use a solution that stores memoization. In hash table, store an array at each key, and iterate through the array, adding each possible combo to the final array
 
 # Store these combos at each key so over time, it's quick to add every additional possible combination.
+
+def eating_cookies(n, cache=None):
+    hashtable = dict()
+    
+    def memoizer(n, cache):
+        if n == 0:
+            return 1
+
+        if n < 0:
+            return 0
+
+        if n in cache:
+            return cache[n]
+        else:
+            cache[n] = memoizer(n-1, cache) + memoizer(n-2, cache) + memoizer(n-3, cache)
+            return cache[n]
+    
+    return memoizer(n, hashtable)
 
 
 if __name__ == "__main__":
