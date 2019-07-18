0d55ab161db8867b3e9a3c2f4253a5499b81d8a6
Julie Jonak <juliejonak10@gmail.com>
2019-07-18 17:02:20 -0500

    Adds brute force solution for eating_cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,36 +6,27 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-    # Store possible ways to eat the cookie as options to iterate through for solutions [0, 1, 2, 3]
+    # Store possible ways to eat the cookie as options to iterate through for solutions [1, 2, 3]
+    ways_to_eat = [1, 2, 3]
   
     # if there are no cookies, return 1 (only one way to eat 0 cookies)
+    if n == 0:
+        return 1
     # if there are negative cookies, return 0 (not possible to eat negative cookies)
+    if n < 0:
+        return 0
 
-    # iterate through possible ways list:
-        # check all possible solutions with i
-        # subtract i from n
-        # consecutive loops find all possible solutions from new n?
-    
-    # iterate through possible ways list:
-        # check all possible solutions excluding i
+    # the count is equal to the sum of the count of n-1, n-2, and n-3 (all possible ways to start eating the cookies)
+    count = eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
-    # count total possible iterations from both loops
-    # will this include duplicates?
+    return count
 
-    pass
-
-
-
-
+print(eating_cookies(4))
 
 # Could use a solution that stores memoization. In hash table, store an array at each key, and iterate through the array, adding each possible combo to the final array
 
 # Store these combos at each key so over time, it's quick to add every additional possible combination.
 
-# 1: 1 = 1
-# 2: 1 + 1, 2 = 2
-# 3: 1 + 2, 2 + 1, 1 + 1 + 1, 3 = 4
-# 4: 1 + 3, 3 + 1, 1 + 2 + 1, 1 + 1 + 2, 2 + 2, 1 + 1 + 1 + 1, 4 = 7
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
